766e42d814ea84626e84685fd7a126d48864bcd8
ganpeng <ganpeng@outlook.com>
2017-03-11 16:05:11 +0800

    sss

---
+++ phase5/cmake-build-debug/CMakeFiles/phase5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phase5.dir/main.cpp.o"
+  "phase5.pdb"
+  "phase5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phase5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
